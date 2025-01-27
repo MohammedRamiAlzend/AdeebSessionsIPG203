ad9f4b3af926a75f3e00038484b27ad29bfe1a42
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-01-27 20:39:15 +0300

    end of Session 1

--- AdeebSessionsIPG203/Program.cs
+++ AdeebSessionsIPG203/Program.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace AdeebSessionsIPG203
 {
+    internal class Adeeb
+    {
+        private static void DisplayAdeebName()
+        {
+            Program program = new Program();
+            Console.WriteLine("Adeeb");
+            program.IsOdd(10);
+            program.DisplayHello();
+        }
+    }
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            Console.Beep();
         }
+       public void DisplayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+       public bool IsEven(int num)
+        {
+            bool result = num % 2 == 0;
+            return result;
+        }
+       public void IsOdd(int num)
+        {
+            bool result = num % 2 == 1;
+            Console.WriteLine(result);
+        }
+        /*
+         * to declare function or method
+         * [AccessModifire] [static or not static] ReturnType MethodName(parameters)
+         * {
+         * 
+         * }
+         * where i can declare a function?
+         * any were in class scoop
+         */
+        /*
+         * How I can declare an Object?
+         * int x = 10;
+         * DataType name = new Datatype();
+         * Person adeeb = new Person("Adeeb","mansour",24 , etc...);
+         */
     }
 }
