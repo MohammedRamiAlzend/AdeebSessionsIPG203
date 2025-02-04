23bddb26600d89ab2eb78355a733a211d1c4a085
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-02-04 15:37:54 +0300

    end of session 2

--- AdeebSessionsIPG203/Program.cs
+++ AdeebSessionsIPG203/Program.cs
@@ -3,14 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using RamiSessionsIPG203;
 namespace AdeebSessionsIPG203
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Student student = new Student();
+            student.SetName("Rami");
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+            Console.WriteLine(student.GetName());
+
+            student.SetName("Adeeb");
+            Console.WriteLine(student.GetName());
         }
-       
+    }
+    class Student
+    {
+        private string name;
+
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+        public string GetName()
+        {
+            return name;
+        }
     }
 }
