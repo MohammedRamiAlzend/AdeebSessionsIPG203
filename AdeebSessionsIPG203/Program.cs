3ef984cfb1af0750e8e3ffb578782a9e9e5e0db9
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-02-17 21:40:48 +0300

    get set

--- AdeebSessionsIPG203/Program.cs
+++ AdeebSessionsIPG203/Program.cs
@@ -1,44 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdeebSessionsIPG203
 {
-    class StudentClass
+    class Student
     {
-        public string Name { get; set; }
-    }
-    struct StudentStruct
-    {
-        public string Name { get; set; }
-    }
+        int id;
+        public int Id
+        {
+            get => id;
 
+            set => id = value;
+        }
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Name { get; set; }
+        public void PrintInfo()
+        {
+            Id = 10;
+            Console.WriteLine("The name is {0}, The age is: {1}", Name, Age);
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------- Referance Type ----------------");
-            StudentClass student = new StudentClass();
-            student.Name = "Adeeb";
-            Console.WriteLine(student.Name);
+            int[] array = { 1, 2, 3, 4 };
 
-            StudentClass student2 = student;
-            student2.Name = "Rami";
-            Console.WriteLine(student.Name);
-            Console.WriteLine("--------------- Referance Type ----------------");
-            /*********************************************/
-            Console.WriteLine("/**********************************************/");
-            Console.WriteLine("--------------- Value Type ----------------");
-            StudentStruct studentstruct = new StudentStruct();
-            studentstruct.Name = "Adeeb";
-            Console.WriteLine(studentstruct.Name);
-
-            StudentStruct student2struct = studentstruct;
-            student2struct.Name = "Rami";
-            Console.WriteLine(studentstruct.Name);
-            Console.WriteLine("--------------- Value Type ----------------");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
 
         }
     }
