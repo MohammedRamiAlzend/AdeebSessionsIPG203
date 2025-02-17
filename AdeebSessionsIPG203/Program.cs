a0394cc2f3cb1c1893f6179f7b94d39e4a61654f
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-02-17 21:18:42 +0300

    understanding the diffirance between value type and referance type

--- AdeebSessionsIPG203/Program.cs
+++ AdeebSessionsIPG203/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace AdeebSessionsIPG203
 {
+    class StudentClass
+    {
+        public string Name { get; set; }
+    }
+    struct StudentStruct
+    {
+        public string Name { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------- Referance Type ----------------");
+            StudentClass student = new StudentClass();
+            student.Name = "Adeeb";
+            Console.WriteLine(student.Name);
+
+            StudentClass student2 = student;
+            student2.Name = "Rami";
+            Console.WriteLine(student.Name);
+            Console.WriteLine("--------------- Referance Type ----------------");
+            /*********************************************/
+            Console.WriteLine("/**********************************************/");
+            Console.WriteLine("--------------- Value Type ----------------");
+            StudentStruct studentstruct = new StudentStruct();
+            studentstruct.Name = "Adeeb";
+            Console.WriteLine(studentstruct.Name);
+
+            StudentStruct student2struct = studentstruct;
+            student2struct.Name = "Rami";
+            Console.WriteLine(studentstruct.Name);
+            Console.WriteLine("--------------- Value Type ----------------");
+
         }
     }
 }
