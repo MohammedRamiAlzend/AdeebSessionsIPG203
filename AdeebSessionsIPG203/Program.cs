be56801f07040fb5089be0b7607bbc932ce07973
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-02-19 21:55:01 +0300

    review conceptes (constructer - properties - fields - Methods)

--- AdeebSessionsIPG203/Program.cs
+++ AdeebSessionsIPG203/Program.cs
@@ -7,23 +7,74 @@ using System.Threading.Tasks;
 
 namespace AdeebSessionsIPG203
 {
+    class Student
+    {
+        //declare fields 
+        private string id;
+        private string name;
+        private int age;
+        private string email;
+        private decimal salary;
+        private bool hasSalary;
+        //default constructer
+        public Student()
+        {
+            id = "";
+            name = "";
+            age = 0;
+            email = "";
+            salary = 0;
+            hasSalary = false;
+        }
+        //overloaded consturecters
+        public Student(string vId, string name, int age, string email, decimal salary)
+        {
+            id = vId;
+            this.name = name;
+            this.age = age;
+            this.email = email;
+            this.salary = salary;
+            hasSalary = true;
+        }
+        public Student(string vId, string name, int age, string email)
+        {
+            id = vId;
+            this.name = name;
+            this.age = age;
+            this.email = email;
+            hasSalary= false;
+        }
+        //declare properties
+        public decimal Salary { get => salary; set => salary = value; }
+        public string Id { get => id; set => id = value; }
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public void PrintInformations()
+        {
+            Console.WriteLine($"The Id is :{id}");
+            Console.WriteLine($"The Name is :{name}");
+            Console.WriteLine($"The Age is :{age}");
+            Console.WriteLine($"The Email is :{email}");
+            if (hasSalary == true)
+                Console.WriteLine($"The Salary is :{Salary}");
+        }
+
+    }
     internal class Program
     {
-        int x;
-        public Program()
-        {
-            x = 55;
-        }
-        public Program(int number)
-        {
-            x= number;
-        }
         static void Main(string[] args)
         {
-            Program o2 = new Program();
-            Program o = new Program(33);
-            Console.WriteLine(o2.x);
-            Console.WriteLine(o.x);
+            Student student = new Student("21", "Rami", 24, "ramialzend@gmail.com", 0);
+            student.PrintInformations();
+            Console.WriteLine("---------------------------------------------");
+            Student student2 = new Student("22", "adeeb", 24, "adeebmansour@gmail.com");
+            student2.PrintInformations();
+
         }
     }
+
 }
+
+
+
