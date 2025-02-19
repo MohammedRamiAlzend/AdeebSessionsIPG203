67674d2a207046b37aa1c9e59911433c056af74d
MohammedRamiAlzend <162128732+MohammedRamiAlzend@users.noreply.github.com>
2025-02-19 21:16:45 +0300

    simple constructer

--- AdeebSessionsIPG203/Program.cs
+++ AdeebSessionsIPG203/Program.cs
@@ -7,43 +7,23 @@ using System.Threading.Tasks;
 
 namespace AdeebSessionsIPG203
 {
-    class Student
-    {
-        // [private] DT name;
-        int id;
-        public Hero hero;
-        public int Id
-        {
-            get => id;
-
-            set => id = value;
-        }
-        private int age;
-        public int Age
-        {
-            get { return age; }
-            set { age = value; }
-        }
-        public string Name { get; set; }
-        public void PrintInfo()
-        {
-            Id = 10;
-            Console.WriteLine("The name is {0}, The age is: {1} {2}", Name, Age , hero.Id);
-        }
-
-    }
-
-    class Hero
-    {
-        public int Id { get; set; }
-    }
     internal class Program
     {
+        int x;
+        public Program()
+        {
+            x = 55;
+        }
+        public Program(int number)
+        {
+            x= number;
+        }
         static void Main(string[] args)
         {
-            Student student = new Student();
-            student.hero = new Hero();
-            student.PrintInfo();    
+            Program o2 = new Program();
+            Program o = new Program(33);
+            Console.WriteLine(o2.x);
+            Console.WriteLine(o.x);
         }
     }
 }
