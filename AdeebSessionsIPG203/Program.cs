be847e2ae1250166cd8c4891af4205826dec063a
أديب منصور < @MSI>
2025-06-10 18:17:38 +0300

    end of session 6

--- AdeebSessionsIPG203/Program.cs
+++ AdeebSessionsIPG203/Program.cs
@@ -7,54 +7,13 @@ using System.Threading.Tasks;
 
 namespace AdeebSessionsIPG203
 {
-    class Person
-    {
-        public static int instances = 0;
-        private string name;
-        public static string Organisation { get; set; }
-        public int GetInstances()
-        {
-            return instances;
-        }
-        public static void WritePersonNumber()
-        {
-            Console.WriteLine("Using WritePersonNumber, Person Counter is : {0} ",
-           instances);
-        }
-        public Person(string name)
-        {
-            instances++;
-            this.name = name;
-        }
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public  void PrintInfo()
-        {
-            Console.WriteLine(instances);
-        }
-
-
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Test t = new Test();
-            t.MyValue = 1;
+            Console.WriteLine(Math.Sqrt(25)); 
 
-            Test t2 = new Test();
-            t2.MyValue = 3;
-
-            Console.WriteLine(t.MyValue);
         }
-
-    }
-    public class Test
-    {
-        public int MyValue { get; set; }
     }
 }
 
---
+++ AdeebSessionsIPG203/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdeebSessionsIPG203
+{
+    internal class Rectangle
+    {
+        public static int myint = 10;
+        int length;
+        int width;
+        public int Length
+        {
+            get { return length; }
+            set
+            {
+                if (value < 0)
+                    length = 0;
+                else
+                    length = value;
+            }
+        }
+        public int Width
+        {
+            get { return width; }
+            set { width = value < 0 ? 0 : value; }
+        }
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public int Area()
+        {
+            return (length + width) * 2;
+        }
+        public int Perimeter()
+        {
+            return (length * width);
+        }
+    }
+}
